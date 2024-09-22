706f6fd10bc6769d2499f9be6402c2cf86c55d61
Fatih YILDIZ <fth.yldz@outlook.com.tr>
2024-09-23 01:29:44 +0300

    Solution by Fatih YILDIZ

--- Integration/Service/ItemIntegrationService.cs
+++ Integration/Service/ItemIntegrationService.cs
@@ -1,10 +1,13 @@
-﻿using Integration.Common;
+﻿using System.Collections.Concurrent;
+using Integration.Common;
 using Integration.Backend;
 
 namespace Integration.Service;
 
 public sealed class ItemIntegrationService
 {
+    private ConcurrentDictionary<string, bool> _itemCache = new ConcurrentDictionary<string, bool>();
+
     //This is a dependency that is normally fulfilled externally.
     private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
 
@@ -14,15 +17,25 @@ public sealed class ItemIntegrationService
     // be allowed for performance reasons.
     public Result SaveItem(string itemContent)
     {
-        // Check the backend to see if the content is already saved.
+        // To prevent another thread from trying to add the same content at the same time.
+        if (!_itemCache.TryAdd(itemContent, true))
+            return new Result(false, $"Another request is processing the item with content {itemContent}.");
+
+        // Check if content already exists.
         if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+            return new Result(false, $"Item with content {itemContent} already exists.");
+
+        try
         {
-            return new Result(false, $"Duplicate item received with content {itemContent}.");
+            // Save new item.
+            var item = ItemIntegrationBackend.SaveItem(itemContent);
+            return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
         }
-
-        var item = ItemIntegrationBackend.SaveItem(itemContent);
-
-        return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+        finally
+        {
+            // When the saving process is complete, we remove the content from the cache.
+            _itemCache.TryRemove(itemContent, out _);
+        }
     }
 
     public List<Item> GetAllItems()
